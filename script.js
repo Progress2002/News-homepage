155132e4c40dd06ff06a71dafa235ae9773ab848
Progress C. Ezeamaka <ezeamakachisom2002@gmail.com>
2022-12-27 16:14:04 +0100

    Create mobile version

---
+++ script.js
@@ -0,0 +1,11 @@
+const mobileNav = document.getElementById('mobile-nav')
+const navToggleBtn = document.querySelector('#hamburger')
+const overlay = document.querySelector('#overlay')
+
+const toggle = () => {
+  mobileNav.classList.toggle('active');
+  navToggleBtn.classList.toggle('active');
+  overlay.classList.toggle('active');
+}
+
+navToggleBtn.addEventListener('click', toggle)
